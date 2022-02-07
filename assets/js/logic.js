7ad8a139b7d0ef5a2efb5ee57a21d4e1c20cb6f1
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2022-02-07 15:50:58 -0500

    fixed index.html and updated logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,10 @@
-//variables to keep track of quiz state
+// variables to keep track of quiz state
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
-//variables to reference DOM elements
-var questionEl =document.getElementById("questions");
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
@@ -13,31 +13,32 @@ var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
 function startQuiz() {
-    var startScreenEl = document.getElementbyId("start-screen");
+    // hide start screen
+    var startScreenEl = document.getElementById("start-screen");
     startScreenEl.setAttribute("class", "hide");
-
-    questionsEl.removeAttribute("class", "hide");
-
+  
+    // un-hide questions section
+    questionsEl.removeAttribute("class");
+  
     // start timer
     timerId = setInterval(clockTick, 1000);
-
+  
     // show starting time
     timerEl.textContent = time;
-
+  
     getQuestion();
-}
-
-function getQuestion() {
-
-// get current question
-var currentQuestion = questions[currentQuestionIndex];
-
-// update title with current question
-var titleEl = document.getElementById("question-title");
-titleEl.textContent = currentQuestion.title;
-
-// clear old question choices
-choicesEl.innerHTML ="";
+  }
+  
+  function getQuestion() {
+    // get current question object from array
+    var currentQuestion = questions[currentQuestionIndex];
+  
+    // update title with current question
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+  
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
 
   // loop over choices
   currentQuestion.choices.forEach(function(choice, i) {

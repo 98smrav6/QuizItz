a6377de52904b04a5ed074e65636e108a962b244
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2022-02-07 15:51:40 -0500

    added questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+      title: "What does API stand for?",
+      choices: ["All Programming Interface", "Application People Interest", "All Programming Internet", "Application Programming Interface"],
+      answer: "Application Programming Interface"
+    },
+    {
+      title: "Third party APIs are not built into your browser.",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "What is it called when the browser collects all of the information for that event and packages it into an object for us to use?",
+      choices: [
+        "event object",
+        "event interface",
+        "event programming",
+        "event saving"
+      ],
+      answer: "event interface"
+    },
+    {
+      title:
+        "What does event.preventDefault() do?",
+      choices: ["It stops the browser from reloading the page upon a form submission.",
+    "It stops the browser from allowing the form submission event to occur."],
+      answer: "It stops the browser from reloading the page upon a form submission."
+    },
+    {
+      title:
+        "The browser event submit allows us to do the following:",
+      choices: ["Submit a form using a button.", "Submit a form using the Enter Key.", "Submit a form using both a button and the Enter Key."],
+      answer: "Submit a form using both a button and the Enter Key."
+    }
+  ];
+  
+
+
